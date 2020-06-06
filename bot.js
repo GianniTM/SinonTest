07913e8b5851dd22e6c6fea6496b4e34b196b9ba
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-06 19:30:49 +0200

    pingssssss

--- bot.js
+++ bot.js
@@ -18,14 +18,22 @@ for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
-if (!client.commands.has(command)) return;
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-try {
-    client.commands.get(command).execute(message, args);
-} catch (error) {
-    console.error(error);
-    message.reply('there was an error trying to execute that command!');
-}
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+    // do the same for the rest of the commands...
+});
 // MAKE VARIABLES OF IMPORTED JS FILES END //
 ///////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////
