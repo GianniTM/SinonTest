5d3ef42cd0c51a3a89c597bedc948728b4025f4b
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-06 20:59:48 +0200

    every command is a file

--- commands/RussianRoulette.js
+++ commands/RussianRoulette.js
@@ -1,3 +1,15 @@
+// MAKE VARIABLES OF IMPORTED JS FILES START //
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { ErelaClient } = require('erela.js')
+const YTDL = require('ytdl-core');
+const { stripIndents } = require("common-tags")
+var search = require('youtube-search');
+var opts = {
+    maxResults: 10,
+    key: process.env.YT_TOKEN
+};
 module.exports = {
     name: 'rr',
     description: 'rr!',
