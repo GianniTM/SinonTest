e83a171f58b0000979c0564f51e13df014c1ae60
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-07 01:13:28 +0200

    duration

--- commands/Play.js
+++ commands/Play.js
@@ -53,6 +53,16 @@ module.exports = {
                     }
                     var server = servers[message.guild.id];
                     mentionMessage = message.content.slice(3);
+                    async function testAll() {
+                        const video1 = await youtube.getVideo("https://www.youtube.com/watch?v=5NPBIwQyPWE");
+                        const video2 = await youtube.getVideoByID("5NPBIwQyPWE");
+                        const video3 = await youtube.searchVideos("big poppa biggie smalls");
+                        const videoArray1 = await youtube.getPlaylist("https://www.youtube.com/playlist?list=PLxyf3paml4dNMlJURcEOND0StDN1Q4yWz");
+                        const videoArray2 = await youtube.getPlaylistByID("PLxyf3paml4dNMlJURcEOND0StDN1Q4yWz");
+
+                        console.log(video1, video2, video3, videoArray1, videoArray2);
+                    }
+                    testAll();
                     message.member.voiceChannel.join().then(connection =>{
                         const video = youtube.searchVideos(mentionMessage)
                             mentionMessage = video;
