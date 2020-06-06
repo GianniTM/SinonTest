451379853d20425ea16ac1b703153ae385dd2e85
Gianni <r0739251@student.thomasmore.be>
2020-06-06 02:46:20 +0200

    =p clean

--- bot.js
+++ bot.js
@@ -168,7 +168,7 @@ client.on('message', async message => {
     }
     // playing + queueing song
     else if (message.content.startsWith('=p ')){
-        //music.Plays(message);
+        music.Plays(message);
     }
     // gif your game react
     else if(message.content.startsWith('https://www.gifyourgame.com/'))
