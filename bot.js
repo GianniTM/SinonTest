9ac9afc0b8dabff97207563425e8fb0198c7ca20
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-06 19:28:26 +0200

    skip + pause added + reaction

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 // MAKE VARIABLES OF IMPORTED JS FILES START //
+const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { ErelaClient } = require('erela.js')
@@ -10,6 +11,21 @@ var opts = {
     maxResults: 10,
     key: process.env.YT_TOKEN
 };
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+if (!client.commands.has(command)) return;
+
+try {
+    client.commands.get(command).execute(message, args);
+} catch (error) {
+    console.error(error);
+    message.reply('there was an error trying to execute that command!');
+}
 // MAKE VARIABLES OF IMPORTED JS FILES END //
 ///////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////
