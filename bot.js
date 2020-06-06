a1da3f380e775bc1658c090bb978a70b3c18510c
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-06 19:36:13 +0200

    pingssssss

--- bot.js
+++ bot.js
@@ -19,14 +19,7 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
 }
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (command === 'ping') {
-        client.commands.get('ping').execute(message, args);
-    }
     // do the same for the rest of the commands...
 });
 // MAKE VARIABLES OF IMPORTED JS FILES END //
@@ -660,6 +653,18 @@ client.on('ready', () => {
 // COMMANDS START //
 client.on('message',message => {
     //getting sinon quotes
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
     if (message.content === prefix + 'q')
     {
         Quote(message);
