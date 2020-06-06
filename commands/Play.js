4c100384603dab9c587bc34e4bf617f741644b56
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-07 01:01:35 +0200

    duration

--- commands/Play.js
+++ commands/Play.js
@@ -10,7 +10,8 @@ var opts = {
     maxResults: 10,
     key: process.env.YT_TOKEN
 };
-const { getVideoDurationInSeconds } = require('get-video-duration')
+const YouTube = require("discord-youtube-api");
+const youtube = new YouTube(process.env.YT_TOKEN);
 module.exports = {
     name: 'p',
     description: 'p!',
@@ -18,7 +19,7 @@ module.exports = {
         function Play(connection, message)
         {
             var server = servers[message.guild.id];
-            server.dispatcher = connection.playStream(YTDL(server.queue[0].link, {filter: "audioonly"}));
+            server.dispatcher = connection.playStream(YTDL(server.queue[0].url, {filter: "audioonly"}));
             server.dispatcher.on("end",function () {
                 server.queue.shift();
                 if (server.queue[0]){
@@ -53,17 +54,12 @@ module.exports = {
                     var server = servers[message.guild.id];
                     mentionMessage = message.content.slice(3);
                     message.member.voiceChannel.join().then(connection =>{
-                        search(mentionMessage, opts, function(err, results) {
-                            if(err) return console.log(err);
-                            mentionMessage = results[0];
-                            const title = results[0].title;
+                        const video = youtube.searchVideos(mentionMessage)
+                            mentionMessage = video;
+                            const title = video.title;
                             const embed = new Discord.RichEmbed();
                             embed.setAuthor("Now Playing:", message.author.displayAvatarURL);
                             embed.setTitle(title);
-                            const stream = fs.createReadStream(results[0].link)
-                            getVideoDurationInSeconds(stream).then((duration) => {
-                                message.channel.send(duration);
-                            })
                             message.channel.send({embed}).then(m => {
                                 server.dispatcher.on("end",function () {
                                     m.delete()
