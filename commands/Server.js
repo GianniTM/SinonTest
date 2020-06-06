d2515f7ce2a456b50b9c29c2ccbedef67cc8b78d
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-06 20:49:22 +0200

    every command is a file

--- commands/Server.js
+++ commands/Server.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'server',
     description: 'server',
     execute(message, args) {
-        const embed = new Discord.RichEmbed();
+        const embed = new Discord.MessageEmbed();
         embed.setTitle(message.guild.name);
         embed.setThumbnail(message.guild.iconURL)
         embed.setDescription(`Current Members: ${message.guild.memberCount}`);
