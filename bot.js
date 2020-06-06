30963582c971efd95a00ef096d35db785b48e37b
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-06 17:45:18 +0200

    skip + pause added + reaction

--- bot.js
+++ bot.js
@@ -580,11 +580,12 @@ function Play(connection, message)
 function Skip(message)
 {
     var server = servers[message.guild.id];
-    if (!server || !server.queue[0]){
-        message.channel.send("No song's currently playing")}
-    else{
+    if (!server || !server.queue[0]) {
+        message.channel.send("No song's currently playing")
+    } else {
         server.dispatcher.end();
         message.react("▶");
+    }
 }
 // SKIP SONG END //
 ////////////////////////////////////////////////////////////////////////////////////////
