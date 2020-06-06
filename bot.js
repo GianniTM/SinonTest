caf8d3bc97de44eab7aef925875649dce4e935e1
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-06 19:33:19 +0200

    pingssssss

--- bot.js
+++ bot.js
@@ -24,13 +24,8 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if (!client.commands.has(command)) return;
-
-    try {
-        client.commands.get(command).execute(message, args);
-    } catch (error) {
-        console.error(error);
-        message.reply('there was an error trying to execute that command!');
+    if (command === 'ping') {
+        client.commands.get('ping').execute(message, args);
     }
     // do the same for the rest of the commands...
 });
