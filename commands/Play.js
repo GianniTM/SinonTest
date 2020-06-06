e80b2ea50d09c8557140ae71082805c3ed3493a7
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-07 01:16:28 +0200

    duration

--- commands/Play.js
+++ commands/Play.js
@@ -57,10 +57,8 @@ module.exports = {
                         const video1 = await youtube.getVideo("https://www.youtube.com/watch?v=5NPBIwQyPWE");
                         const video2 = await youtube.getVideoByID("5NPBIwQyPWE");
                         const video3 = await youtube.searchVideos("big poppa biggie smalls");
-                        const videoArray1 = await youtube.getPlaylist("https://www.youtube.com/playlist?list=PLxyf3paml4dNMlJURcEOND0StDN1Q4yWz");
-                        const videoArray2 = await youtube.getPlaylistByID("PLxyf3paml4dNMlJURcEOND0StDN1Q4yWz");
 
-                        console.log(video1, video2, video3, videoArray1, videoArray2);
+                        console.log(video1, video2, video3);
                     }
                     testAll();
                     message.member.voiceChannel.join().then(connection =>{
