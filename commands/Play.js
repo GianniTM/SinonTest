a1578b0260d6c925127eb6fa5b8b16ec82da9026
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-07 21:26:22 +0200

    playlist private

--- commands/Play.js
+++ commands/Play.js
@@ -24,7 +24,14 @@ module.exports = {
                 const embed = new Discord.RichEmbed();
                 embed.setAuthor("Playlist:", message.author.displayAvatarURL);
                 embed.setDescription( "Added 0 songs! Your Playlist is private!");
-                const videoArray1 = await youtube.getPlaylist(mentionMessage)
+                try{
+                    const videoArray1 = await youtube.getPlaylist(mentionMessage);
+                }
+                catch(error){
+                    message.channel.send({embed});
+                    return;
+                }
+                const videoArray1 = await youtube.getPlaylist(mentionMessage);
                 message.member.voiceChannel.join().then(connection =>{
                 for(song of videoArray1) {
                     mentionMessage = song;
