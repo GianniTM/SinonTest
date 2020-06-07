73d8d2c57d19910bf75dee385c191be3f8ee6b83
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-07 15:01:21 +0200

    coin toss added

---
+++ commands/coinflip.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { ErelaClient } = require('erela.js')
+const YTDL = require('ytdl-core');
+const { stripIndents } = require("common-tags")
+var search = require('youtube-search');
+var opts = {
+    maxResults: 10,
+    key: process.env.YT_TOKEN
+};
+module.exports = {
+    name: 'cf',
+    description: 'coinflip!',
+    execute(message, args) {
+        var i = Math.floor(Math.random() * 2 + 1);
+        if(i == '1'){
+            const embed = new Discord.RichEmbed();
+            embed.setTitle('Coin Toss')
+            embed.setDescription("The coin landed on Heads!")
+            var randomImg = "heads.png";
+            embed.setThumbnail({files: [randomImg]});
+            message.channel.send({embed});
+        }
+        else{
+            const embed = new Discord.RichEmbed();
+            embed.setTitle('Coin Toss')
+            embed.setDescription("The coin landed on Tails!")
+            var randomImg = "tails.png";
+            embed.setThumbnail({files: [randomImg]});
+            message.channel.send({embed});
+        }
+
+    },
+};
