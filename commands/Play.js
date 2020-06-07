24c2877dacb51530a295d0ea5465c5a2c4c97d67
Gianni Andries <r0739251@student.thomasmore.be>
2020-06-07 10:40:58 +0200

    links added

--- commands/Play.js
+++ commands/Play.js
@@ -17,7 +17,13 @@ module.exports = {
     description: 'p!',
     execute(message, args) {
         async function testAll(mentionMessage) {
-            const video = await youtube.searchVideos(mentionMessage)
+            if (mentionMessage.startsWith("https://www.youtube.com/watch?")){
+                var video = await youtube.getVideo(mentionMessage);
+            }
+            else{
+                var video = await youtube.searchVideos(mentionMessage);
+            }
+
             message.member.voiceChannel.join().then(connection =>{
                 mentionMessage = video;
                 const title = video.title;
@@ -44,7 +50,7 @@ module.exports = {
                 if (server.queue[0]){
                     const title = server.queue[0].title;
                     const embed = new Discord.RichEmbed();
-                    embed.setAuthor("Now Playing:", "https://images-ext-2.discordapp.net/external/C5rK2371x-fIsGosTVXQo1IzhaKIXpe6ol9Zgk8KrIw/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/713003111945470013/0a883c7fe46b95b79b79e2e7a0021d5b.png?width=677&height=677");
+                    embed.setAuthor("Now Playing:", message.author.displayAvatarURL);
                     embed.setDescription(title)
                     message.channel.send({embed}).then(m => {
                         server.dispatcher.on("end",function () {
@@ -99,7 +105,13 @@ module.exports = {
                     } else {
                         QueueAll(mentionMessage)
                         async function QueueAll(mentionMessage) {
-                            const video = await youtube.searchVideos(mentionMessage)
+
+                            if (mentionMessage.startsWith("https://www.youtube.com/watch?")){
+                                var video = await youtube.getVideo(mentionMessage);
+                            }
+                            else{
+                                var video = await youtube.searchVideos(mentionMessage);
+                            }
                             message.member.voiceChannel.join().then(connection =>{
                                 mentionMessage = video;
                                 const title = video.title;
