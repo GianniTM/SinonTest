7244f03b99eac98af884c9920fc0edf72c8f96bc
GianniTM <r0739251@student.thomasmore.be>
2021-06-03 14:35:18 +0200

    add more details

--- commands/Avatar.js
+++ commands/Avatar.js
@@ -36,7 +36,7 @@ module.exports = {
                 '**Joined Discord**',new Date(mention.createdTimestamp).toLocaleDateString()
             );
             embed.addField(
-                '**Role Count**',member.roles.cache
+                '**Role Count**',member.roles
             );
             embed.setColor("37bceb");
             message.channel.send({embed});
