280c4505eab9d6419eb6e1d22d68030b98b090cb
GianniTM <r0739251@student.thomasmore.be>
2021-06-03 14:57:05 +0200

    Highest Role$

--- commands/Avatar.js
+++ commands/Avatar.js
@@ -36,7 +36,7 @@ module.exports = {
                 '**Joined Discord**',new Date(mention.createdTimestamp).toLocaleDateString()
             );
             embed.addField(
-                '**Highest Role**',member.roles.member
+                '**Highest Role**',member.roles.highest.name
             );
             embed.setColor("37bceb");
             message.channel.send({embed});
